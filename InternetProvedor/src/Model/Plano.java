f4d8527fa48505b5011ab873c0e6251a488a5f62
ViniciusAClement <clementevinicius13@gmail.com>
2025-05-06 15:16:32 -0300

    3 comiit teste

--- InternetProvedor/src/Model/Plano.java
+++ InternetProvedor/src/Model/Plano.java
@@ -3,4 +3,46 @@ package Model;
 public class Plano {
 	private int megas;
 	private double preco;
+	
+	private static class Builder{
+		private int megas;
+		private double preco;
+		
+		public Builder megas ( int megas ) {
+			this.megas = megas;
+			return this;
+		}
+		
+		public Builder preco ( double preco ) {
+			this.preco = preco;
+			return this;
+		}
+		
+		public Plano builder() {
+			Plano plano = new Plano();
+			
+			plano.megas = this.megas;
+			plano.preco = this.preco;
+			
+			return plano;
+		}
+	}
+
+	public int getMegas() {
+		return megas;
+	}
+
+	public void setMegas(int megas) {
+		this.megas = megas;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
 }
--- InternetProvedor/src/Model/PlanoDoCliente.java
+++ InternetProvedor/src/Model/PlanoDoCliente.java
@@ -3,4 +3,27 @@ package Model;
 public class PlanoDoCliente {
 	private Plano plano;
 	private boolean status;
+	
+	private static class Builder {
+		private Plano plano;
+		private boolean status;
+		
+		public Builder plano ( Plano plano ) {
+			this.plano = plano;
+			return this;
+		}
+		
+		public Builder status ( boolean status ) {
+			this.status = status;
+			return this;
+		}
+		
+		public PlanoDoCliente builder() {
+			PlanoDoCliente planoDoCliente = new PlanoDoCliente();
+			planoDoCliente.plano = this.plano;
+			planoDoCliente.status = this.status;
+			
+			return planoDoCliente;
+		}
+	}
 }
